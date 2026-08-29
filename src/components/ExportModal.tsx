19fa378ba135709ca25898d3debfed27da0d8233
fesdinang <fesdinang@gmail.com>
2026-08-29 17:05:40 +0700

    fix(export): Correct HTML entity escaping in escapeHtml function

--- src/components/ExportModal.tsx
+++ src/components/ExportModal.tsx
@@ -4,11 +4,11 @@ import { X, FileText, Printer, Check, Download } from 'lucide-react';
 
 export function escapeHtml(str: string): string {
   return str
-    .replace(/&/g, "&amp;")
-    .replace(/</g, "&lt;")
-    .replace(/>/g, "&gt;")
-    .replace(/"/g, "&quot;")
-    .replace(/'/g, "&#39;");
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#39;');
 }
 
 interface ExportModalProps {
